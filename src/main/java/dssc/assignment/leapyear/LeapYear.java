cc2b667d2ff23bc38ec41ef50dcfcc77b27fd8de
MatteoStromieri <matteostromieri@outlook.it>
2023-11-22 17:50:42 +0100

    refactor LeapYear.print(int n)

--- src/main/java/dssc/assignment/leapyear/LeapYear.java
+++ src/main/java/dssc/assignment/leapyear/LeapYear.java
@@ -1,14 +1,11 @@
 package dssc.assignment.leapyear;
 
 public class LeapYear {
-    public void print(int i) {
-        if(i==1)
-            System.out.println("False");
-        else if(i==4)
+    public void print(int k) {
+        if(k%4==0 & (k % 100 != 0 || k % 400 == 0)){
             System.out.println("True");
-        else if(i==100)
-            System.out.println("False");
+        }
         else
-            System.out.println("True");
+            System.out.println("False");
     }
 }
